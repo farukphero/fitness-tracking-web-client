91814f9408bca3387b5554067d5ab1c0a78dfc5f
farukphero <farukk6319@gmail.com>
2023-01-12 00:11:33 +0600

    routes setup

---
+++ src/Pages/OtherPages/Foods/Foods.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Foods = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Foods;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,18 +1,50 @@
 import { createBrowserRouter } from "react-router-dom";
+import KeepTrack from "../../Layout/KeepTrack/KeepTrack";
 import Main from "../../Layout/Main/Main";
 import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
 import Home from "../../Pages/Home/Home/Home";
+import Community from "../../Pages/OtherPages/Community/Community";
+import Foods from "../../Pages/OtherPages/Foods/Foods";
+import Goals from "../../Pages/OtherPages/Goals/Goals";
+import Leaderboard from "../../Pages/OtherPages/Leaderboard/Leaderboard";
+import Tutorials from "../../Pages/OtherPages/Tutorials/Tutorials";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Main>Hello world!</Main>,
+    element: <Main></Main>,
     errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
-        element: <Home>Hello world!</Home>,
+        element: <Home></Home>,
       },
+      {
+        path: "/Leaderboard",
+        element: <Leaderboard></Leaderboard>,
+      },
+      {
+        path: "/Goals",
+        element: <Goals></Goals>,
+      },
+      {
+        path: "/Community",
+        element: <Community></Community>,
+      },
+      {
+        path: "/Tutorials",
+        element: <Tutorials></Tutorials>,
+      },
+      
     ],
+  },
+  {
+    path: "/KeepTrack",
+    element: <KeepTrack></KeepTrack>,
+    errorElement: <ErrorPage />,
+    children: [{
+        path: "/KeepTrack/Foods",
+        element: <Foods></Foods>,
+    }]
   },
 ]);
