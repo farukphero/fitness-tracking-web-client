84f1bf2f5720b22be4544b93d8d42bc142703412
unknown <mahbubulsohag810@gmail.com>
2023-01-12 14:04:06 +0600

    simple about section

---
+++ src/Pages/Home/AboutUs/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import AboutCarousel from './AboutCarousel';
+
+const About = () => {
+    return (
+        <div className=' bg-gray-700 text-white'>
+            <div className='container mx-auto'>
+            <h3 className='text-4xl font-bold text-center py-10'>About Our Website</h3>
+            <div className='flex gap-2 my-5'>
+                <div className='w-1/2'>
+                    <AboutCarousel />
+                </div>
+                <div className='w-1/2 h-3/4'>
+                    <p className='text-2xl p-5 h-3/4'><span>You can monitor and improve your daily life, activities, and health using this website.</span>
+                    <br/>
+                       <span> You set goals for your diet, sleep, and exercise using the same platform. We track your progress and will update you regularly on your progress.</span>
+                       <br/>
+                        <span>Finally, we will show you the percentage of your goal achieved after the deadline.</span>
+                        <br/>
+                        <span>We hope this site will significantly assist you in maintaining your health and daily activities.</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default About;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import About from '../AboutUs/About';
 import ContactUs from '../ContactUs/ContactUs';
 
 const Home = () => {
     return (
         <div>
             <ContactUs></ContactUs>
+            <About/>
         </div>
     );
 };
