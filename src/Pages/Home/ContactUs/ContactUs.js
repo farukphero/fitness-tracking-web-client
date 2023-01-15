edb73c28a15d072ce162803931edde09a68372d8
niaz abir <niazabir55@gmail.com>
2023-01-15 23:39:39 +0600

    commit thid  by niaz

--- src/Pages/Home/ContactUs/ContactUs.js
+++ src/Pages/Home/ContactUs/ContactUs.js
@@ -1,31 +1,32 @@
 import React from "react";
+import "./Contact.css";
 
 const ContactUs = () => {
   return (
     <section className="mt-12">
-      <h2 className="text-3xl lg:text-4xl text-white font-semibold text-center pt-10">
+      <h2 className="text-3xl lg:text-4xl text-all-green font-semibold text-center pt-10">
         Get In Touch{" "}
       </h2>
-      <p className="text-white text-center lg:w-1/3 mx-auto pt-2 mb-4">
+      <p className="text-white text-center lg:w-1/3 mx-auto pt-2 mb-6">
         {" "}
         Great genius takes shape by contact with another great genius, but, less
         by assimilation than by friction.
       </p>
-      <div className="overflow-hidden bg-gray-700">
-        <div className="px-4  pb-20 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8  ">
-          <div className="w-full xl:px-8 xl:w-6/12 mx-auto">
+      <div className="overflow-hidden ">
+        <div className="px-4   pb-20 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8  ">
+          <div className="w-full border-2 border-green-600 xl:px-8 xl:w-6/12 mx-auto">
             <div className="relative">
-              <div className="relative bg-white rounded shadow-2xl p-7 sm:p-10">
-                <h3 className="mb-4 text-xl text-start sm:mb-6 sm:text-2xl">
+              <div className="relative  rounded shadow-2xl p-7 sm:p-10">
+                <h3 className=" text-xl text-white text-start sm:mb-6 sm:text-2xl">
                   {" "}
-                  Contact form <hr />
+                  Contact Form <hr />
                 </h3>
 
                 <form>
                   <div className="mb-1">
                     <label
                       htmlFor="name"
-                      className="inline-block mb-1 font-medium"
+                      className="inline-block mb-1 text-white font-medium"
                     >
                       Name
                     </label>
@@ -33,7 +34,7 @@ const ContactUs = () => {
                       placeholder="Your Full Name"
                       required
                       type="text"
-                      className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      className="flex-grow w-full h-12 text-white px-4 mb-2 transition duration-200 bg-gray-600 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
                       id="name"
                       name="name"
                     />
@@ -41,32 +42,32 @@ const ContactUs = () => {
                   <div className="mb-1">
                     <label
                       htmlFor="email"
-                      className="inline-block mb-1 font-medium"
+                      className="inline-block mb-1 text-white font-medium"
                     >
                       E-mail
                     </label>
                     <input
-                      placeholder="*******@gmail.com"
+                      placeholder="...@gmail.com"
                       required
                       type="email"
-                      className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-gray-600 text-white rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
                       id="email"
                       name="email"
                     />
                   </div>
                   <div className="mb-1">
-                    <label className="inline-block mb-1 font-medium">
+                    <label className="inline-block text-white mb-1 font-medium">
                       Your Message
                     </label>
                     <textarea
-                      className="textarea textarea-bordered flex-grow w-full   transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      className="textarea textarea-bordered flex-grow w-full   transition duration-200 bg-gray-600 text-white rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline mb-6"
                       placeholder="Your Message"
                     ></textarea>
                   </div>
                   <input
                     type="submit"
                     value="Submit"
-                    className="btn btn-accent text-white w-full"
+                    className="btn btn-contact text-white "
                   />
                 </form>
               </div>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,34 +1,47 @@
-import React from 'react';
-import ServiceCard from './ServiceCard';
+import React from "react";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
-    const servicesData = [
-        {id: 1, 
-        name: 'Diet',
-        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8sMPMzeEUZBprfOs7WTcOqwNTL1-Q8B_3jg&usqp=CAU',
-        description: 'Diet is very important for the health of the human body. You can maintain your daily diet with us.Leave the responsibility of your daily nutrition to us. We will track your progess.'},
-        {id: 2, 
-        name: 'Exercise',
-        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7YSPB7CWGVf1Sue69gD55FzP_Zq8HOvDsHw&usqp=CAU',
-        description: 'Exercise is very important for the health of the human body. You can maintain your daily exercise with us.Leave the responsibility of your daily exercise to us. We will track your progess.'},
-        {id: 3, 
-        name: 'Daily Activities',
-        image:'https://us.123rf.com/450wm/yusufdemirci/yusufdemirci1901/yusufdemirci190100100/yusufdemirci190100100.jpg?ver=6',
-        description: 'Your Daily Activities is very important for the health of you. You can maintain your Daily Activities like eating, sleeping and all everything with us.Leave the responsibility of your Daily Activities to us. We will track your progess.'},
-    ]
-    return (
-       <div className='bg-slate-700 text-white'>
-         <div className='container mx-auto py-10'>
-            <h1 className='text-4xl text-center font-semibold'>This is our services</h1>
-            <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5'>
-                {
-                    servicesData.map(service => <ServiceCard key={service.id}
-                    service={service}></ServiceCard>)
-                }
-            </div>
+  const servicesData = [
+    {
+      id: 1,
+      name: "Diet",
+      image:
+        "https://images.unsplash.com/photo-1535914254981-b5012eebbd15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      description:
+        "Diet is very important for the health of the human body. You can maintain your daily diet with us.Leave the responsibility of your daily nutrition to us. We will track your progess.",
+    },
+    {
+      id: 2,
+      name: "Exercise",
+      image:
+        "https://images.unsplash.com/photo-1517343985841-f8b2d66e010b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80",
+      description:
+        "Exercise is very important for the health of the human body. You can maintain your daily exercise with us.Leave the responsibility of your daily exercise to us. We will track your progess.",
+    },
+    {
+      id: 3,
+      name: "Daily Activities",
+      image:
+        "https://images.unsplash.com/photo-1626143955914-e2c1001bc7d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=781&q=80",
+      description:
+        "Your Daily Activities is very important for the health of you. You can maintain your Daily Activities like eating, sleeping and all everything with us.Leave the responsibility of your Daily Activities to us. We will track your progess.",
+    },
+  ];
+  return (
+    <div className=" text-white">
+      <div className="container mx-auto py-10">
+        <h1 className="text-4xl text-all-green text-center mb-3 font-semibold">
+          Our Services
+        </h1>
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5">
+          {servicesData.map((service) => (
+            <ServiceCard key={service.id} service={service}></ServiceCard>
+          ))}
+        </div>
       </div>
-       </div>
-    );
+    </div>
+  );
 };
 
 export default Services;
