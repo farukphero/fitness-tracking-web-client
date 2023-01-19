e54a68f2abab39b6109914e6f91b47a41f90ba37
sohag <mahbubulsohag810@gmail.com>
2023-01-20 02:10:58 +0600

    load service and user data from backend and load service details

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./ServiceCard.css";
 
 const ServiceCard = ({ service }) => {
-  const { id, name, description, image } = service;
+  const { _id, name, description, image } = service;
   // console.log(service);
   return (
     <div className="w-full my-10  shadow-xl image-full rounded relative">
@@ -18,7 +18,7 @@ const ServiceCard = ({ service }) => {
         <h2 className="card-title text-all-green font-semibold">{name}</h2>
         <p>{description.slice(0, 170)}...</p>
         <div>
-         <Link to={`/serviceDetails/${id}`} state={{ from: {service} }}> <button className="border-2 px-6 py-3 border-green-600 btn-explore  rounded-md ">
+         <Link to={`/serviceDetails/${_id}`} state={{ from: {service} }}> <button className="border-2 px-6 py-3 border-green-600 btn-explore  rounded-md ">
             Explore More
           </button></Link>
         </div>
