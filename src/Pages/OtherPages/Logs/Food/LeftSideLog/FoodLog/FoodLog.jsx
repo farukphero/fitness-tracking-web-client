9c0b7b5385b9c1b101212abf4efca7f97d48e310
sohag <mahbubulsohag810@gmail.com>
2023-01-24 01:47:09 +0600

    working on how much in food in log

--- src/Pages/OtherPages/Logs/Food/LeftSideLog/FoodLog/FoodLog.jsx
+++ src/Pages/OtherPages/Logs/Food/LeftSideLog/FoodLog/FoodLog.jsx
@@ -1,19 +1,48 @@
 import React from "react";
 import { useState } from "react";
-// import { useForm } from "react-hook-form";
 
 const FoodLog = () => {
-  // const { register, handleSubmit } = useForm();
   const [foodValue, setFoodValue] = useState('');
+  const [foodData, setFoodData] = useState({});
+  const [foodAmount, setFoodAmount] = useState('');
+  // console.log(foodAmount)
+  console.log(foodValue)
 
-  const data = [{ name: 'Rice' }, { name: 'Tea' }, { name: 'Toast' }]
+ const data = [
+      {
+        category: "Beef & Veal Calories",
+        name: "beef",
+        cal_per_oz: 48,
+        per_serving: {
+          type: "ounce",
+          amount: 6,
+          gram: 168
+        },
+        amount :[{amount:"1 kg"}, {amount:"1 cup"}, {amount:"1 piece"}]
+        }
+    ]
+    console.log(data)
 
   const handleOnChange = event => {
     setFoodValue(event.target.value);
   }
 
+  const handleAmountChange = event =>{
+   
+  }
+
   const onSearch = searchTerm => {
-    setFoodValue(searchTerm)
+    setFoodValue(searchTerm.name)
+    setFoodData(searchTerm)
+  }
+
+  const handleAmount = a =>{
+    if(foodValue){
+      setFoodAmount(a.amount)
+    }
+    else if(!foodValue){
+      setFoodAmount('')
+    }
   }
 
   const handleFoodLogForm = (value) => {
@@ -22,62 +51,7 @@ const FoodLog = () => {
 
   return (
     <>
-      {/* <div className="card-body border rounded-md">
-        <div className="flex items-center justify-between">
-          <h2 className="font-bold text-4xl capitalize">food log</h2>
-        </div>
-        <form onSubmit={handleSubmit(handleFoodLogForm)}>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text text-white font-semibold capitalize text-2xl">
-                what did you eat?
-              </span>
-            </label>
-            <input
-              type="text"
-              className="input bg-gray-500 input-bordered focus:outline-none"
-              {...register(`foodName`)}
-            />
-          </div>
-          <div className="flex items-center justify-between space-x-3">
-            <div className="form-control basis-2/3">
-              <label className="label">
-                <span className="label-text text-white font-semibold text-2xl capitalize">
-                  how much?
-                </span>
-              </label>
-              <input
-                type="text"
-                className="input input-md input-bordered bg-gray-500 focus:outline-none"
-                {...register(`amount`)}
-              />
-            </div>
-            <div className="form-control basis-1/3">
-              <label className="label">
-                <span className="label-text font-semibold capitalize text-white text-2xl">
-                  when?
-                </span>
-              </label>
-              <select {...register(`time`)} className="select bg-gray-500 input-md select-bordered">
-                <option disabled selected>
-                  Anytime
-                </option>
-                <option>Breakfast</option>
-                <option>Morning Snack</option>
-                <option>Launch</option>
-                <option>Afternoon Snack</option>
-                <option>Dinner</option>
-                <option>After Dinner</option>
-              </select>
-            </div>
-          </div>
-          <div className="form-control mt-6">
-            <button className="btn btn-contact capitalize">Log</button>
-          </div>
-        </form>
-      </div> */}
-
-      <div className="card-body border rounded-md">
+       <div className="card-body border rounded-md">
         <div className="flex items-center justify-between">
           <h2 className="font-bold text-4xl capitalize">food log</h2>
         </div>
@@ -103,7 +77,8 @@ const FoodLog = () => {
                 const foodName = item.name.toLowerCase();
                 return searchTerm && foodName.startsWith(searchTerm) && foodName !== searchTerm;
               }).map(item => <div className=" bg-slate-200 w-1/2"
-                onClick={() => onSearch(item.name)}
+                // onClick={() => onSearch(item.name)}
+                 onClick={() => onSearch(item)}
                 key={item.name}
               >
                 <p className="text-black p-3 mt-1">{item.name}</p>
@@ -111,6 +86,7 @@ const FoodLog = () => {
             </div>
           </div>
           <div className="flex items-center justify-between space-x-3">
+            <div>
             <div className="form-control basis-2/3">
               <label className="label">
                 <span className="label-text text-white font-semibold text-2xl capitalize">
@@ -121,7 +97,18 @@ const FoodLog = () => {
                 type="text"
                 className="input input-md input-bordered bg-gray-500 focus:outline-none"
                 name="amount"
+                value={foodAmount}
+                onChange={handleAmountChange}
               />
+            </div>
+            <div>
+              {foodData?.amount?.map(a =><div className=" bg-slate-200 w-1/2"
+                 onClick={() => handleAmount(a)}
+                key={a.amount}
+              >
+                 {!foodAmount &&  <p className="text-black p-3 mt-1">{a.amount}</p>}
+              </div>)}
+            </div>
             </div>
             <div className="form-control basis-1/3">
               <label className="label">
