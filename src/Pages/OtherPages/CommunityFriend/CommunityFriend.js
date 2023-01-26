0d50e35df5d7e34447b575fe474e4f0655649b39
sadekinchowdhury <rumelchow340@gmail.com>
2023-01-26 15:54:01 +0600

    feed

---
+++ src/Pages/OtherPages/CommunityFriend/CommunityFriend.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommunityFriend = () => {
+    return (
+        <div>
+            <h1>amar</h1>
+        </div>
+    );
+};
+
+export default CommunityFriend;
---
+++ src/Pages/OtherPages/CommunityModal/CommunityModal.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import CommunityFeed from '../Communityfeed/CommunityFeed';
+
+const CommunityModal = () => {
+    return (
+        <div>
+            {/* The button to open modal */}
+            <label htmlFor="my-modal-3" className="btn">Post start</label>
+
+            {/* Put this part before </body> tag */}
+            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box relative">
+                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <CommunityFeed></CommunityFeed>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CommunityModal;
