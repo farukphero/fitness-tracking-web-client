d89956c1e06a71394998a9cfb3d8fc3c0bb2d210
farukphero <farukk6319@gmail.com>
2023-02-23 16:16:13 +0600

    niaz vaiyer code

--- src/Pages/Dashboard/UserInfo/UserInfo.js
+++ src/Pages/Dashboard/UserInfo/UserInfo.js
@@ -8,7 +8,7 @@ import useTitle from "../../../Hooks/useTitle/useTitle";
 import "./UserInfo.css";
 
 const UserInfo = () => {
-  useTitle("Profile/userInfo");
+  useTitle("Profile/UserInfo");
   const {
     register,
     formState: { errors },
@@ -102,8 +102,9 @@ const UserInfo = () => {
 
   // intake of water daily:
   const getWater = (weight) => {
-    let intake = [Number(weight) / 30];
-    return intake;
+    let intake = Number(weight) / 30;
+    const result = intake.toFixed(2)
+    return result;
   };
 
   const water = getWater(userInfo?.weight);
@@ -111,7 +112,6 @@ const UserInfo = () => {
   // blood measurement :
   const getBlood = (weight) => {
     let measurement = [(Number(weight) * 8) / 100];
-
     return measurement;
   };
 
@@ -126,76 +126,76 @@ const UserInfo = () => {
   const protein = getProtein(userInfo?.weight);
 
   // bmr :
-  const calculateBMR = (bmrData) => {
-    let { age, gender, heightFeet, heightInches, weight, weightType } = bmrData;
+  // const calculateBMR = (bmrData) => {
+  //   let { age, gender, heightFeet, heightInches, weight, weightType } = bmrData;
 
-    if (
-      age === "" ||
-      gender === "" ||
-      heightFeet === "" ||
-      heightInches === "" ||
-      weight === ""
-    ) {
-      return null;
-    }
+  //   if (
+  //     age === "" ||
+  //     gender === "" ||
+  //     heightFeet === "" ||
+  //     heightInches === "" ||
+  //     weight === ""
+  //   ) {
+  //     return "null";
+  //   }
 
-    let bmrCalc = "";
-    let height = heightFeet * 30.48 + heightInches * 2.54;
+  //   let bmrCalc = "";
+  //   let height = heightFeet * 30.48 + heightInches * 2.54;
 
-    if (weightType == 1) {
-      if (gender == "Female") {
-        bmrCalc = 66 + 6.2 * weight + 12.7 * height - 6.76 * age;
-      } else if (gender == "Male") {
-        bmrCalc = 655.1 + 4.35 * weight + 4.7 * height - 4.7 * age;
-      }
-    } else if (weightType == 2) {
-      if (gender == "Female") {
-        bmrCalc = 66.5 + 13.75 * weight + 5.003 * height - 6.755 * age;
-      } else if (gender == "Male") {
-        bmrCalc = 655 + 9.563 * weight + 1.85 * height - 4.676 * age;
-      }
-    }
-    return bmrCalc;
-  };
+  //   if (weightType === 1) {
+  //     if (gender === "Female") {
+  //       bmrCalc = 66 + 6.2 * weight + 12.7 * height - 6.76 * age;
+  //     } else if (gender === "Male") {
+  //       bmrCalc = 655.1 + 4.35 * weight + 4.7 * height - 4.7 * age;
+  //     }
+  //   } else if (weightType === 2) {
+  //     if (gender === "Female") {
+  //       bmrCalc = 66.5 + 13.75 * weight + 5.003 * height - 6.755 * age;
+  //     } else if (gender === "Male") {
+  //       bmrCalc = 655 + 9.563 * weight + 1.85 * height - 4.676 * age;
+  //     }
+  //   }
+  //   return bmrCalc;
+  // };
 
   return (
-    <div className="w-8/12 mx-auto my-20 -mt-5 lg:mt-20 scale-75 lg:scale-100">
+    <div className="w-8/12 mx-auto my-20 -mt-5 md:-mt-80 lg:mt-20 scale-75 lg:scale-100">
       <div className="lg:w-[900px] ">
-        <div className="-ml-24 ">
-          <h1 className="text-2xl mb-4">Health Activities:</h1>
-          <div className="grid grid-cols-3 lg:grid-cols-5">
-            <div className="indicator mt-4">
-              <span className="indicator-item badge bg-white text-2xl px-4 py-4 badge-primary">
-                {/* {bmi} */} 24.4
+        <div className="-ml-24 mb-5">
+          <h1 className="text-2xl my-8">Health Activities:</h1>
+          <div className="grid grid-cols-4 lg:grid-cols-4 gap-20">
+            <div className="indicator ">
+              <span className="indicator-item badge bg-white text-2xl px-2 py-3 md:p-4 badge-primary">
+                {bmi} 
               </span>
-              <div className="grid w-24 h-24 bg-gradient-to-r from-gray-600 via-teal-700 to-gray-600   place-items-center">
+              <div className="grid w-20 h-20 md:w-24 md:h-24 bg-gradient-to-r from-gray-600 via-teal-700 to-gray-600 place-items-center">
                 <Link to="/bmi"> BMI</Link>
               </div>
             </div>
 
-            <div className="indicator mt-4">
-              <span className="indicator-item badge bg-white text-2xl px-4 py-4 badge-primary">
+            <div className="indicator ">
+              <span className="indicator-item badge bg-white text-2xl px-2 py-3 md:p-4 badge-primary">
                 {blood}L
               </span>
-              <div className="grid w-24 h-24 pl-4 bg-gradient-to-r from-gray-600 via-teal-700 to-gray-600   place-items-center">
+              <div className="grid w-20 h-20 md:w-24 md:h-24 pl-4 bg-gradient-to-r from-gray-600 via-teal-700 to-gray-600   place-items-center">
                 Include Blood
               </div>
             </div>
-            <div className="indicator mt-4">
-              <span className="indicator-item badge bg-white text-2xl px-4 py-4 badge-primary">
+            <div className="indicator ">
+              <span className="indicator-item badge bg-white text-2xl px-2 py-3 md:p-4 badge-primary">
                 {protein}G
               </span>
-              <div className="grid w-24 h-24 pl-4 bg-gradient-to-r from-gray-600 via-teal-700 to-gray-600   place-items-center">
+              <div className="grid w-20 h-20 md:w-24 md:h-24 pl-4 bg-gradient-to-r from-gray-600 via-teal-700 to-gray-600   place-items-center">
                 Intake Protein
               </div>
             </div>
-            <div className="indicator mt-4">
+            {/* <div className="indicator mt-4">
               <span className="indicator-item badge bg-white text-2xl px-4 py-4 badge-primary">
                 {calculateBMR({
                   age: userInfo?.age,
                   gender: userInfo?.gender,
-                  heightFeet: userInfo.height,
-                  heightInches: userInfo.inch,
+                  heightFeet: userInfo?.height,
+                  heightInches: userInfo?.inch,
                   weight: userInfo?.weight,
                   weightType: 1,
                 })}
@@ -203,13 +203,13 @@ const UserInfo = () => {
               <div className="grid w-24 h-24 bg-gradient-to-r from-gray-600 via-teal-700 to-gray-600   place-items-center">
                 <Link to="/bmr"> BMR</Link>
               </div>
-            </div>
-            <div className="indicator mt-4">
-              <span className="indicator-item badge bg-white text-2xl px-4 py-4 badge-primary">
-                {/* {water} */}
-                1.667L
+            </div> */}
+            <div className="indicator ">
+              <span className="indicator-item badge bg-white text-2xl px-2 py-3 md:p-4 badge-primary">
+                {water}
+                {/* 1.667L */}
               </span>
-              <div className="grid w-24 h-24 pl-4 bg-gradient-to-r from-gray-600 via-teal-700 to-gray-600 place-items-center">
+              <div className="grid w-20 h-20 md:w-24 md:h-24 pl-4 bg-gradient-to-r from-gray-600 via-teal-700 to-gray-600 place-items-center">
                 Intake Water
               </div>
             </div>
@@ -245,7 +245,7 @@ const UserInfo = () => {
                     <ImFolderPlus className="h-6 w-7 p-1" />
                   </label>
                 </div>
-                <div className="flex lg:gap-8 -ml-28 lg:ml-0 gap-2 mt-3">
+                <div className="flex md:gap-8 -ml-28 md:ml-0 gap-2 mt-3">
                   <div>
                     <label>First name</label>
                     <input
@@ -266,7 +266,7 @@ const UserInfo = () => {
                   </div>
                 </div>
 
-                <div className="flex lg:gap-8  -ml-28 lg:ml-0 gap-2">
+                <div className="flex md:gap-8  -ml-28 md:ml-0 gap-2">
                   <div>
                     <label>birthday</label>
                     <input
@@ -316,8 +316,8 @@ const UserInfo = () => {
                     />
                   </div>
                 </div>
-                <div className="flex lg:gap-12 gap-2 mr-20 w-56">
-                  <button className="btn btn-explore bg-gradient-to-r from-gray-700 via-green-700 to-gray-700 btn-outline mt-5 w-full">
+                <div className="flex md:gap-12 gap-2 mr-20 w-46 lg:w-60">
+                  <button className="btn btn-log bg-secondary text-black mt-5 w-full">
                     save
                   </button>
                   <button
